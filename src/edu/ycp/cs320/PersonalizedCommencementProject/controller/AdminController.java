0fe99983f66d8a7b7d2064d2501324d5ece92d19
Alyssa Grove <agrove9@ycp.edu>
2019-04-08 07:41:49 -0400

    Added get/set EndDate methods in admin model and admin controller

--- src/edu/ycp/cs320/PersonalizedCommencementProject/controller/AdminController.java
+++ src/edu/ycp/cs320/PersonalizedCommencementProject/controller/AdminController.java
@@ -1,9 +1,26 @@
 package edu.ycp.cs320.PersonalizedCommencementProject.controller;
 
+import java.util.Date;
+
+import edu.ycp.cs320.PersonalizedCommencementProject.model.Admin;
+
+
 // Admin Controller class
 /*
  * TODO: Everything
  */
 public class AdminController {
+	private Admin model;
 
+	public Admin getModel() {
+		return model;
+	}
+	
+	public void setModel(Admin model) {
+		this.model = model;
+	}
+	
+	public void setEndDate(Date date) {
+		model.setEndDate(date);
+	}
 }
--- src/edu/ycp/cs320/PersonalizedCommencementProject/model/Admin.java
+++ src/edu/ycp/cs320/PersonalizedCommencementProject/model/Admin.java
@@ -1,10 +1,13 @@
 package edu.ycp.cs320.PersonalizedCommencementProject.model;
 
+import java.util.Date;
+
 // Admin Model class
 /*
  * TODO: Everything
  */
 public class Admin extends User{
+	private Date date;
 	
 	public Admin() {
 		
@@ -13,4 +16,13 @@ public class Admin extends User{
 	public Admin(User user) {
 		super(user.getUsername(), user.getPassword(), user.getType(), user.getFirstName(), user.getLastName());
 	}
+	
+	public Date getEndDate() {
+		return date;
+	}
+	
+	public void setEndDate(Date date) {
+		this.date = date;
+	}
+	
 }
