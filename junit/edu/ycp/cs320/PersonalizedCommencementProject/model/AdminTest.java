0ebfc6fb3b2d7edb28472736db23075e23dca942
jgross11 <jgross11@ycp.edu>
2019-04-12 08:46:12 -0400

    Removed unnecessary tests, updated date testing

--- junit/edu/ycp/cs320/PersonalizedCommencementProject/model/AdminTest.java
+++ junit/edu/ycp/cs320/PersonalizedCommencementProject/model/AdminTest.java
@@ -7,12 +7,6 @@ import java.util.Date;
 import org.junit.Before;
 import org.junit.Test;
 
-/*
- * TODO: These tests are incomplete as the Graduate class currently (3/10)
- * TODO: contains only a portion of the attributes/functions in its class diagram
- * TODO: As such, these tests are incomplete and will need to be updated when the 
- * TODO: other classes (admin, advisor, etc.) are implemented. 
- */
 
 // JUnits for Graduate
 public class AdminTest {
@@ -26,72 +20,17 @@ public class AdminTest {
 	}
 	
 	@Test
-	public void TestSetandGetNumOfcompletedAdvisors() {
-		model1.setNumOfCompletedAdvisors(10);
-		model2.setNumOfCompletedAdvisors(54);
-		model3.setNumOfCompletedAdvisors(108);
-		
-		assertEquals(model1.getNumOfCompletedAdvisors(),10); 
-		assertEquals(model2.getNumOfCompletedAdvisors(),54); 
-		assertEquals(model3.getNumOfCompletedAdvisors(),108); 
-		
-	}
-	
-	@Test
-	public void TestSetandGetNumberOfAdvisors() {
-		model1.setNumAdvisors(10);
-		model2.setNumAdvisors(54);
-		model3.setNumAdvisors(108);
-		
-		assertEquals(model1.getNumAdvisors(),10); 
-		assertEquals(model2.getNumAdvisors(),54); 
-		assertEquals(model3.getNumAdvisors(),108);
-		
-	}
-	
-	@Test
-	public void TestSetandGetAdvisorsandAddAdvisors() {
-		Advisor[] Advisor1 = new Advisor[3];
-		Advisor[] Advisor2 = new Advisor[31];
-		Advisor[] Advisor3 = new Advisor[22];
-		
-		model1.setAdvisors(Advisor1);
-		model2.setAdvisors(Advisor2);
-		model3.setAdvisors(Advisor3);
-		
-		assertTrue(model1.getAdvisors().equals(Advisor1)); 
-		assertTrue(model2.getAdvisors().equals(Advisor2)); 
-		assertTrue(model3.getAdvisors().equals(Advisor3)); 
-		
-	}
-	
-	@Test
 	public void TestSetandGetDate() {
-		Date d1 = new Date(2002,1,3);
-		Date d2 = new Date(2009,5,25); 
-		Date d3 = new Date(2011,6,29); 
+		long d1 = 200;
+		long d2 = 20000; 
+		long d3 = 20000000; 
 		
-		model1.setDate(2002, 1, 3);
-		model2.setDate(2009, 5, 25);
-		model3.setDate(2011, 6, 29);
+		model1.setDate(d1);
+		model2.setDate(d2);
+		model3.setDate(d3);
 		
-		assertEquals(model1.getDate(),d1); 
-		assertEquals(model2.getDate(),d2); 
-		assertEquals(model3.getDate(),d3); 	
+		assertEquals(model1.getDate().getTime(),d1); 
+		assertEquals(model2.getDate().getTime(),d2); 
+		assertEquals(model3.getDate().getTime(),d3); 	
 	}
-	
-	//@Test
-	//public void TestAddAdvisorandGetCertainAdvaisor() {
-		//Advisor Dave = new Advisor(); 
-		//Advisor Willma = new Advisor(); 
-		//Advisor Jess = new Advisor(); 
-		
-		//model1.addAdvisor(0, Dave);
-		//model2.addAdvisor(0, Willma);
-		//model3.addAdvisor(0, Jess);
-		
-		//assertTrue(model1.getCertainAdvisor(0).equals(Dave)); 
-		//assertTrue(model2.getCertainAdvisor(0).equals(Willma)); 
-		//assertTrue(model3.getCertainAdvisor(0).equals(Jess)); 
-	//}
 }
