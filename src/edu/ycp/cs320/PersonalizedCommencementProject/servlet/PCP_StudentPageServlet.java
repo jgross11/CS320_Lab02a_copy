0cbfe538cebb584151d8cf4a79411cabcb336170
jgross11 <jgross11@ycp.edu>
2019-04-13 22:40:48 -0400

    Added session persistence for both graduate and advisor modes

--- src/edu/ycp/cs320/PersonalizedCommencementProject/servlet/PCP_StudentPageServlet.java
+++ src/edu/ycp/cs320/PersonalizedCommencementProject/servlet/PCP_StudentPageServlet.java
@@ -6,18 +6,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import edu.ycp.cs320.PersonalizedCommencementProject.controller.GraduateController;
 import edu.ycp.cs320.PersonalizedCommencementProject.model.Advisor;
 import edu.ycp.cs320.PersonalizedCommencementProject.model.Graduate;
 import edu.ycp.cs320.PersonalizedCommencementProject.model.User;
-
-// TODO navigating between login / student pages is working 90% of the time
-// TODO find / fix cases where it doesn't
-
-// TODO finally, understand that the graduate and graduateController classes 
-// TODO are not complete at this time (3/9) since they only contain a small 
-// TODO amount of the attributes the Graduate class diagram has 
 
 public class PCP_StudentPageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -32,31 +26,32 @@ public class PCP_StudentPageServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		System.out.println("PCP_StudentPage Servlet: doGet");
-		
-		// the supplied login information is valid,
-		// call JSP to generate student page
+		System.out.println("PCP_AdvisorPage Servlet : doGet");
+		// validate user should be allowed to access this page by checking
+		// the type of user associated with the provided username
+		String username = "";
 		try{
-			System.out.println("Attemping to navigate...");
-			if(req.getAttribute("validLogIn").equals(true)) {
-				System.out.println("Navigating to student page");
-				resp.sendRedirect("http://localhost:8081/PersonalizedCommencementProject/PCP_StudentPage");
-			}
-			// the supplied login information is not valid,
-			// return user to login page
-
-			else {
-				System.out.println("Navigating to login page due to bad data");
-				resp.sendRedirect("http://localhost:8081/PersonalizedCommencementProject/PCP_Index");
-				//req.getRequestDispatcher("/_view/PCP_Index.jsp").forward(req,  resp);
-			}
+			username = req.getSession().getAttribute("username").toString();
 		}
-		// no information is supplied,
-		// return user to login page
-		catch(NullPointerException e){
-			System.out.println("Navigating to login page due to no data");
-			resp.sendRedirect("http://localhost:8081/PersonalizedCommencementProject/PCP_Index");
-			//req.getRequestDispatcher("/_view/PCP_Index.jsp").forward(req,  resp);
+		catch(NullPointerException e) {
+
+		}
+		// TODO: make this search userDB for session informations usernames' type
+		// TODO: and redirect to student page only if type is student or advisor
+		
+		// allow students to always access this page
+		if(username.equals("dchism") || username.equals("wabram")) {
+			req.getRequestDispatcher("/_view/PCP_StudentPage.jsp").forward(req, resp);
+		}
+		
+		// allow advisors to access this page so long
+		// as they have a student's information to view
+		else if(username.equals("agrove9") && req.getSession().getAttribute("studentToView") != null){
+			req.getRequestDispatcher("/_view/PCP_StudentPage.jsp").forward(req, resp);
+		}
+		// direct access w/o logging in, or admin account type
+		else {
+			resp.sendRedirect(req.getContextPath() + "/PCP_Index");
 		}
 	}
 
@@ -74,10 +69,13 @@ public class PCP_StudentPageServlet extends HttpServlet {
 		
 		// creates controller that holds provided graduate's Graduate model
 		GraduateController controller = new GraduateController();
-
-		// decode POSTed form parameters and dispatch to controller
-		// stores inputed username and password
-		String studentName = req.getParameter("studentName");
+		
+		// HttpSession allows for local persistence while 
+		// the user is interacting with the project
+		HttpSession session = req.getSession();
+		
+		// store name of student for use across session
+		String studentName = session.getAttribute("studentName").toString();
 		
 		// once the database is set up, this would iterate through every student in the database
 		// and load information to the page only if the supplied information is valid (found in 
@@ -101,29 +99,50 @@ public class PCP_StudentPageServlet extends HttpServlet {
 		
 		// this has to change once InfoState and ContentComponent are implemented correctly, but
 		// for now it can be assumed that if the graduate name is correct, so is this information.
-		String studentAcademicInformation = req.getParameter("studentAcademicInformation");
-		String studentExtraInformation = req.getParameter("studentExtraInformation");
+		String studentAcademicInformation = session.getAttribute("studentAcademicInformation").toString();
+		String studentExtraInformation = session.getAttribute("studentExtraInformation").toString();
 		
-		// These can be used later on to differentiate the choice of the student
-		String studentSaveChanges = req.getParameter("studentSaveChanges");
-		String mode = req.getParameter("mode");
+		// determines whether or not student wishes
+		// to save changes to their media selections
+		String studentSaveChanges = "";
 		
-		if(mode.equals("graduateView")) {
-			req.setAttribute("mode", "graduateEdit");
-			System.out.println("Switching graduate mode from view to edit");
+		// mode the JSP should display
+		String mode = "";
+		
+		// attempt to obtain the student's action
+		// null if student was just redirected to page
+		try{
+			studentSaveChanges = req.getParameter("studentSaveChanges").toString();
+			mode = session.getAttribute("mode").toString();
 		}
+		catch(NullPointerException e) {
+			// only null for the first post, safe to assume
+			// user wants to edit information
+			mode = "graduateView";
+			System.out.println("Switching graduate mode from view to edit - First time");
+		}
+		
+		// student wishes to edit their media
+		if(mode.equals("graduateView")) {
+			session.setAttribute("mode", "graduateEdit");
+			System.out.println("Switching graduate mode from view to edit");
+			req.getRequestDispatcher("/_view/PCP_StudentPage.jsp").forward(req, resp);
+		}
+		
+		// student wishes to either save or discard their
+		// changes to their media
 		else if(mode.equals("graduateEdit")){
 			System.out.println(req.getParameter("graduateLayoutChange"));
-			if(req.getParameter("graduateLayoutChange").equals("true")){
+			if(req.getParameter("graduateLayoutChange").toString().equals("true")){
 				// TODO: set graduate's layout mode to the chosen value
 				// TODO: this is temporary; will eventually change model
-				req.setAttribute("graduateLayout", req.getParameter("graduateLayout"));
-				req.setAttribute("graduateLayoutChange", "false");
-				req.setAttribute("mode", "graduateEdit");
+				session.setAttribute("graduateLayout", req.getParameter("graduateLayout"));
+				session.setAttribute("graduateLayoutChange", "false");
+				session.setAttribute("mode", "graduateEdit");
 				System.out.println("Returning to graduate edit mode after modifying layout");
 			}
 			else {
-				req.setAttribute("mode", "graduateView");
+				session.setAttribute("mode", "graduateView");
 				System.out.println("Switching graduate mode from edit to view");
 				if(studentSaveChanges.equals("true")) {
 					System.out.println("\n\nStudent wishes to save changes");
@@ -139,16 +158,29 @@ public class PCP_StudentPageServlet extends HttpServlet {
 					System.out.println("Invalid Values, save: " + studentSaveChanges);
 				}
 			}
+			req.getRequestDispatcher("/_view/PCP_StudentPage.jsp").forward(req, resp);
 		}
+		// advisor wishes to dis/approve a student's media selections
 		else if(mode.equals("advisorView")){
-			if(req.getParameter("advisorSwitch").equals("true")) {
-				req.setAttribute("mode", "advisorEdit");
-				System.out.println("Switching advisor mode from view to edit");
-				req.setAttribute("advisorUsername", req.getParameter("advisorUsername"));
+			try {
+				// attempt to get requested values and update session values
+				session.setAttribute("advisorSwitch", req.getParameter("advisorSwitch").toString());
+				session.setAttribute("advisorGoBack", req.getParameter("advisorGoBack").toString());
+				System.out.println(session.getAttribute("advisorGoBack").toString() + ", and " + session.getAttribute("advisorSwitch").toString());
 			}
-			else if(req.getParameter("advisorGoBack").equals("true")) {
+			catch(NullPointerException e) {
+				// first time post case
+				session.setAttribute("advisorGoBack", "true");
+				session.setAttribute("advisorSwitch", "false");
+				System.out.println(session.getAttribute("advisorGoBack").toString() + ", or " + session.getAttribute("advisorSwitch").toString());
+			}
+			if(session.getAttribute("advisorSwitch").toString().equals("true")) {
+				session.setAttribute("mode", "advisorEdit");
+				System.out.println("Switching advisor mode from view to edit");
+				req.getRequestDispatcher("/_view/PCP_StudentPage.jsp").forward(req, resp);
+			}
+			else if(session.getAttribute("advisorGoBack").equals("true")) {
 				System.out.println("Navigating back to advisor home page");
-				req.setAttribute("advisorUsername", req.getParameter("advisorUsername"));
 				advLogin[0] = new Advisor(new User("agrove9", "agrove9", "advisor", "Alyssa", "Grove"));
 				Advisor advisorModel = advLogin[0];
 				advisorModel.setStatus(false);
@@ -166,29 +198,24 @@ public class PCP_StudentPageServlet extends HttpServlet {
 				req.setAttribute("advisorName", advisorModel.getFirstName() + " " + advisorModel.getLastName());
 				req.setAttribute("academicInformation", advisorModel.getAcademicInformation());
 				req.setAttribute("advisorStatus", advisorModel.getStatus());
-				req.getRequestDispatcher("/_view/PCP_AdvisorPage.jsp").forward(req, resp);
+				resp.sendRedirect(req.getContextPath() + "/PCP_AdvisorPage");
 			}
 			
 		}
+		
+		// advisor wishes to save or discard their decisions
+		// regarding student's media choices
 		else if(mode.equals("advisorEdit")) {
-			req.setAttribute("mode", "advisorView");
-			req.setAttribute("advisorUsername", req.getParameter("advisorUsername"));
+			session.setAttribute("mode", "advisorView");
 			System.out.println("Switching advisor mode from edit to view");
 			System.out.println(req.getParameter("advisorSaveChanges"));
 			// TODO: add logic to save dis/approved content
+			
+			req.getRequestDispatcher("/_view/PCP_StudentPage.jsp").forward(req, resp);
 		}
 		else {
 			System.out.println("Invalid mode");
 		}
-		
-		// Add parameters as request attributes
-		// this creates attributes named "first" and "second for the response, and grabs
-		// the
-		// values that were originally assigned to the request attributes, also named
-		// "first" and "second"
-		// they don't have to be named the same, but in this case, since we are passing
-		// them back
-		// and forth, it's a good idea
 		
 		req.setAttribute("studentName", studentName);
 		req.setAttribute("studentAcademicInformation", studentAcademicInformation);
@@ -196,11 +223,5 @@ public class PCP_StudentPageServlet extends HttpServlet {
 		req.setAttribute("model", model);
 		req.setAttribute("studentStatus", model.getStatus());
 		req.setAttribute("studentSaveChanges", studentSaveChanges);
-		
-		// Redirect accordingly
-		// correct graduate information is supplied
-		if(infoInDB) {
-			req.getRequestDispatcher("/_view/PCP_StudentPage.jsp").forward(req, resp);
-		}
 	}
 }
