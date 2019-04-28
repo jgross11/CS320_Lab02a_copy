c4451fd06bd4a67427215c316b9c8775e1cdd6d7
jgross11 <jgross11@ycp.edu>
2019-04-28 10:25:12 -0400

    Added constant ints to be used when g/setting content in states

--- src/edu/ycp/cs320/PersonalizedCommencementProject/databaseModel/InfoState.java
+++ src/edu/ycp/cs320/PersonalizedCommencementProject/databaseModel/InfoState.java
@@ -3,13 +3,24 @@ package edu.ycp.cs320.PersonalizedCommencementProject.databaseModel;
 import java.util.ArrayList;
 
 public class InfoState {
+	public static final int PROFILE_INDEX = 0;
+	public static final int EXTRAINFORMATION_INDEX = 1;
+	public static final int NAMEPRONUNCIATION_INDEX = 2;
+	public static final int SLIDESHOW1_INDEX = 3;
+	public static final int SLIDESHOW2_INDEX = 4;
+	public static final int SLIDESHOW3_INDEX = 5;
+	public static final int SLIDESHOW4_INDEX = 6;
+	public static final int VIDEO_INDEX = 7;
+	private final int numContents = 8;
 	private String formatType;
-	private final int numContents = 7;
 	private String username;
 	private ArrayList<ContentComponent> contents;
 	
 	public InfoState() {
-		contents = new ArrayList<ContentComponent>(numContents);
+		contents = new ArrayList<ContentComponent>();
+		for(int i = 0; i < numContents; i++) {
+			contents.add(new ContentComponent());
+		}
 	}
 	
 	public InfoState(InfoState oldState) {
