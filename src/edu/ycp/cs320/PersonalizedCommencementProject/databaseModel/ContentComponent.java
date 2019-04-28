c2d15d8f2b69a4085cdca45c4bee8cc4360e60a2
jgross11 <jgross11@ycp.edu>
2019-04-28 10:21:48 -0400

    Added InfoState and content type and grad username vars and methods

--- src/edu/ycp/cs320/PersonalizedCommencementProject/databaseModel/ContentComponent.java
+++ src/edu/ycp/cs320/PersonalizedCommencementProject/databaseModel/ContentComponent.java
@@ -1,11 +1,14 @@
 package edu.ycp.cs320.PersonalizedCommencementProject.databaseModel;
 
 public class ContentComponent {
-private boolean status;
+	private boolean status;
 	
 	// photo / video: path to file as a string
 	// text: text (string)
 	private String content;
+	private String username;
+	private String infoStateType;
+	private String type;
 	
 	public ContentComponent() {
 
@@ -14,6 +17,18 @@ private boolean status;
 	public ContentComponent(String content) {
 		this.content = content;
 		status = false;
+	}
+	
+	public ContentComponent(String path, Boolean status) {
+		this.content = path;
+		this.status = status;
+	}
+
+	public ContentComponent(String path, Boolean status, String type, String username) {
+		this.content = path;
+		this.status = status;
+		this.type = type;
+		this.username = username;
 	}
 	
 	public boolean getStatus() {
@@ -30,5 +45,29 @@ private boolean status;
 	
 	public void setContent(String content) {
 		this.content = content;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getInfoStateType() {
+		return infoStateType;
+	}
+
+	public void setInfoStateType(String infoStateType) {
+		this.infoStateType = infoStateType;
 	}
 }
