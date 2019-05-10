8a463724ea308735c75586054ec9f2fc0a8f89e2
jgross11 <jgross11@ycp.edu>
2019-05-10 11:01:32 -0400

    Started to redesign graduate controller for db interaction

--- src/edu/ycp/cs320/PersonalizedCommencementProject/controller/GraduateController.java
+++ src/edu/ycp/cs320/PersonalizedCommencementProject/controller/GraduateController.java
@@ -1,50 +1,36 @@
 package edu.ycp.cs320.PersonalizedCommencementProject.controller;
 
-import edu.ycp.cs320.PersonalizedCommencementProject.miscellaneousClasses.InfoState;
-import edu.ycp.cs320.PersonalizedCommencementProject.model.Advisor;
-import edu.ycp.cs320.PersonalizedCommencementProject.model.Graduate;
+import edu.ycp.cs320.PersonalizedCommencementProject.databaseModel.Advisor;
+import edu.ycp.cs320.PersonalizedCommencementProject.databaseModel.ContentComponent;
+import edu.ycp.cs320.PersonalizedCommencementProject.databaseModel.Graduate;
+import edu.ycp.cs320.PersonalizedCommencementProject.databaseModel.InfoState;
+import edu.ycp.cs320.PersonalizedCommencementProject.persist.DatabaseProvider;
+import edu.ycp.cs320.PersonalizedCommencementProject.persist.DerbyDatabase;
+import edu.ycp.cs320.PersonalizedCommencementProject.persist.IDatabase;
 
 // Graduate controller class
 public class GraduateController extends UserController{
+	IDatabase db = null;
 	private Graduate model;
 	
-	public Graduate getModel() {
-		return model;
+	
+	public GraduateController() {
+		// creates DB instance
+		DatabaseProvider.setInstance(new DerbyDatabase());
+		db = DatabaseProvider.getInstance();
 	}
 	
 	public void setModel(Graduate model) {
 		this.model = model;
 	}
-	
-	public void setMajor(String major) {
-		model.setMajor(major);
+
+	public void insertGraduateMediaIntoContentComponentTable(String username, String string, ContentComponent content) {
+		db.insertGraduateMediaIntoContentComponentTable(username, string, content);
+	}
+
+	public void setInfoAtIndex(int index, ContentComponent content) {
+		model.getPendingInfo().setContentAtIndex(index, content);
 	}
 	
-	public void setMinor(String minor) {
-		model.setMinor(minor);
-	}
 	
-	public void setQRCodeString(String QRCodeString) {
-		model.setQRCodeString(QRCodeString);
-	}
-	
-	public void setStatus(boolean status) {
-		model.setStatus(status);
-	}
-	
-	public void setAdvisor(Advisor advisor) {
-		model.setAdvisor(advisor);
-	}
-	
-	public void setCurrentInfo(InfoState currentInfo) {
-		model.setCurrentInfo(currentInfo);
-	}
-	
-	public void setPendingInfo(InfoState pendingInfo) {
-		model.setPendingInfo(pendingInfo);
-	}
-	
-	public void calculateStatus() {
-		model.calculateStatus();
-	}
 }
