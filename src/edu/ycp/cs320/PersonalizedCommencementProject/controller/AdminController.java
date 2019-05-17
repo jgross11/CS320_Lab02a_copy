7c0464e26b72ff6780b6384486cfc3908a41733f
jgross11 <jgross11@ycp.edu>
2019-05-16 23:05:18 -0400

    Connected admin controller to database

--- src/edu/ycp/cs320/PersonalizedCommencementProject/controller/AdminController.java
+++ src/edu/ycp/cs320/PersonalizedCommencementProject/controller/AdminController.java
@@ -1,12 +1,21 @@
 package edu.ycp.cs320.PersonalizedCommencementProject.controller;
 
-import edu.ycp.cs320.PersonalizedCommencementProject.model.Admin;
-import edu.ycp.cs320.PersonalizedCommencementProject.model.Advisor;
+import edu.ycp.cs320.PersonalizedCommencementProject.databaseModel.Admin;
+import edu.ycp.cs320.PersonalizedCommencementProject.persist.DatabaseProvider;
+import edu.ycp.cs320.PersonalizedCommencementProject.persist.DerbyDatabase;
+import edu.ycp.cs320.PersonalizedCommencementProject.persist.IDatabase;
 
 // Admin Controller class
 
 public class AdminController {
 	private Admin model; 
+	private IDatabase db = null;
+	
+	public AdminController() {
+		// creates DB instance
+		DatabaseProvider.setInstance(new DerbyDatabase());
+		db = DatabaseProvider.getInstance();	
+	}
 	
 	public Admin getModel() {
 		return model;
@@ -18,5 +27,6 @@ public class AdminController {
     
     public void setDate(long date) {
     	model.setDate(date);
+    	db.insertDateIntoAdminTable(date);
     }
 }
